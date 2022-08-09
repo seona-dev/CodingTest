8b010684b532262ef2a1727ecb557b60cfbafffc
seona-dev <seona0826@gmail.com>
2022-08-09 16:34:15 +0900

    Solved 1 Problem! :)

--- baekjoon/src/step8/No1929.java
+++ baekjoon/src/step8/No1929.java
@@ -1,9 +1,27 @@
 package step8;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class No1929 {
 
-	public static void main(String[] args) {
-
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String input = br.readLine();
+		int m = Integer.parseInt(input.split(" ")[0]);
+		int n = Integer.parseInt(input.split(" ")[1]);
+		boolean[] primes = new boolean[n+1];
+		primes[0] = primes[1] = true;
+		for(int i=2;i<=Math.sqrt(n);i++) {
+			if(primes[i]) continue;
+			for(int j=i*i;j<primes.length;j+=i) {
+				primes[j] = true;
+			}
+		}
+		for(int i=m;i<=n;i++) {
+			if(!primes[i]) {
+				System.out.println(i);
+			}
+		}
 	}
-
 }
